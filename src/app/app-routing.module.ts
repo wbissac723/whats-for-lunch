d1dbdf77882afc30cf9a5ec19d25b0500998d23c
SirIsaac <warrenbrown@SirIsaac.local>
2019-01-27 10:08:07 -0600

    Created routes for the components.

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,13 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
+import { LoginComponent } from './components/login/login.component';
+import { NewAccountComponent } from './components/new-account/new-account.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  { path: '', component: LoginComponent },
+  { path: 'login', component: LoginComponent},
+  { path: 'create_account', component: NewAccountComponent},
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,10 @@
 import { Component } from '@angular/core';
+import { Observable } from 'rxjs';
+
+import { Store } from '@ngrx/store';
+import { AppState } from './store';
+import { getLoggedState } from './components/login/store/login.selector';
+
 
 @Component({
   selector: 'app-root',
@@ -6,4 +12,12 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.scss']
 })
 export class AppComponent {
+  public loggedIn: boolean;
+
+  constructor(private store: Store<AppState>) {
+    this.store.select(getLoggedState).subscribe(
+      status => {
+        this.loggedIn = status;
+      });
+  }
 }
