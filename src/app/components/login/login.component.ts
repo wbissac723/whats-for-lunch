ce0e8e6d398b8e90266de7462d024a5c620cfd09
Warren <warrenisaac84@gmail.com>
2019-01-27 07:28:36 -0600

    Subscribed to loggedStatus selector.

--- src/app/components/login/login.component.ts
+++ src/app/components/login/login.component.ts
@@ -1,5 +1,9 @@
 import { Component, OnInit } from '@angular/core';
 import { Store } from '@ngrx/store';
+import { LoginState } from './store';
+
+import { Observable } from 'rxjs';
+import { getLoggedState } from './store/login.selector';
 
 
 @Component({
@@ -8,10 +12,14 @@ import { Store } from '@ngrx/store';
   styleUrls: ['./login.component.scss']
 })
 export class LoginComponent implements OnInit {
+  loggedIn$: Observable<boolean>;
 
-  constructor(private store: Store<any>) {}
+  constructor(private store: Store<LoginState>) { }
 
-  ngOnInit() {}
+  ngOnInit() {
+    this.loggedIn$ = this.store.select(getLoggedState);
+
+  }
 
 
 }
