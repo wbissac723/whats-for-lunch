044b5019e468a5f7058b3ab79188f4a96371c86b
Warren <warrenisaac84@gmail.com>
2019-02-03 10:51:10 -0600

    Created toggle between login and create account.

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -13,13 +13,34 @@ import { Router } from '@angular/router';
 })
 export class AppComponent implements OnInit {
   public loggedIn: boolean;
+  public showLoginText: boolean;
+  public showCreateAccountText: boolean;
+
 
   constructor(private route: Router, private store: Store<AppState>) {
     this.store.select(getLoggedState)
       .subscribe((status: boolean) => this.loggedIn = status);
   }
 
-  ngOnInit() { }
+  ngOnInit() {
+    this.showLoginText = false;
+    this.showCreateAccountText = true;
+  }
+
+  showCreateAccount() {
+    this.showLoginText = true;
+    this.showCreateAccountText = false;
+
+    this.route.navigate(['/create_account']);
+  }
+
+  showLogin() {
+    this.showLoginText = false;
+    this.showCreateAccountText = true;
+
+    this.route.navigate(['/login']);
+
+  }
 
   logout() {
     this.route.navigate(['/login']);
