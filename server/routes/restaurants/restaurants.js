37c0e50be49e9a9cff57b623a701a508f9456f09
Warren <warrenisaac84@gmail.com>
2019-02-16 21:59:40 -0600

    Added comments to api GET request.

--- server/routes/restaurants/restaurants.js
+++ server/routes/restaurants/restaurants.js
@@ -11,20 +11,30 @@ const bearerToken = 'a63vBF5N-p5D2w05KaYF78UVcBV99NK1P6Z3tyVsWZLGHcSWays53-lnAOs
 router.get('/', async (req, res) => {
 
   const options = {
-      uri: req.body.url,
+
+      uri: req.body.url,  // yelp search url sent from the client
       headers: {
-        'Authorization': 'Bearer ' + bearerToken
+        'Authorization': 'Bearer ' + bearerToken  // api key
       }
+
   };
 
+  // Sends GET request to the YELP api to retrieve list of restaurants
   request(options)
+
     .then((data) => {
+      console.log('Successfully retrieved restaurants.')
+
       res.status(200).send(data);
     })
+
     .catch((err) => {
       console.log('Call failed: ' + JSON.stringify(err, null, 3));
-      res.status(500).send('Could not locate resource');
+
+      res.status(500).send('Unable to locate restaurants.');
+
     });
+
 });
 
 module.exports = router;
