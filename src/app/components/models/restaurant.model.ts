77197b21973f812c0e2ac8ebdfc813e5df5efd68
SirIsaac <warrenbrown@SirIsaac.local>
2019-02-17 19:54:17 -0600

    Displaying search results of restaurants.

--- src/app/components/models/restaurant.model.ts
+++ src/app/components/models/restaurant.model.ts
@@ -1,6 +1,16 @@
 export class Restaurant {
     name: string;
-    address: string;
+    isClosed: boolean;
+    location: Address;
     category: string;
     hoursOfOperation: string;
+    phoneNumber: string;
+    rating: number;
+    website?: string;
+    imgURL?: string;
+}
+
+export class Address {
+    line1: string;
+    line2: string;
 }
--- src/app/components/user-account/components/user-hub/user-hub.component.ts
+++ src/app/components/user-account/components/user-hub/user-hub.component.ts
@@ -3,6 +3,7 @@ import { FormBuilder, FormGroup, Validators, AbstractControl } from '@angular/fo
 
 import { LocatorService } from '../../../services/locator/locator.service';
 import { SearchParams } from 'src/app/components/services/locator/search-params/search-params.model';
+import { Restaurant } from 'src/app/components/models/restaurant.model';
 @Component({
   selector: 'app-user-hub',
   templateUrl: './user-hub.component.html',
@@ -12,6 +13,8 @@ export class UserHubComponent implements OnInit {
 
   searchForm: FormGroup;
   searchQuery: SearchParams = new SearchParams();
+  restaurants: Restaurant[];
+  searchSuccess: boolean;
 
   constructor(
     private locator: LocatorService,
@@ -45,11 +48,32 @@ export class UserHubComponent implements OnInit {
     this.locator.getRestaurants(this.searchQuery)
       .subscribe(
         (response) => {
+          this.restaurants = this.createListOfRestaurants(response);
+          this.searchSuccess = true;
           console.log('Successfully retrieved restaurants');
         },
         (error) => {
+          this.searchSuccess = false;
           console.log('Service call failed. ' + JSON.stringify(error, null, 3));
         });
+  }
+
+  createListOfRestaurants(response: any[]): Restaurant[] {
+    const restaurants = [];
+
+    response.forEach(obj => {
+      const restaurant = new Restaurant();
+
+      restaurant.imgURL = obj.image_url;
+      restaurant.isClosed = obj.is_closed;
+      restaurant.name = obj.name;
+      restaurant.phoneNumber = obj.display_phone;
+      restaurant.rating = obj.rating;
+
+      restaurants.push(restaurant);
+    });
+
+    return restaurants;
   }
 
 }
