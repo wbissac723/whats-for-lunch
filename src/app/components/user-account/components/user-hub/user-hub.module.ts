6423824ab0663f7d4670d570807f1bb920a83b7a
chauhansaurabh2 <chuahansaurabh2@gmail.com>
2019-02-18 14:35:50 -0600

    Use restraunt component on the user component.

--- src/app/components/user-account/components/user-hub/user-hub.module.ts
+++ src/app/components/user-account/components/user-hub/user-hub.module.ts
@@ -5,13 +5,15 @@ import { UserHubComponent } from './user-hub.component';
 import { ReactiveFormsModule } from '@angular/forms';
 import { HTTP_INTERCEPTORS } from '@angular/common/http';
 import { FoodLocatorInterceptor } from '../../../interceptors/food-locator.interceptor';
+import { RestaurantModule } from '../../../restaurant/restaurant.module';
 
 @NgModule({
   declarations: [UserHubComponent],
   exports: [UserHubComponent],
   imports: [
     CommonModule,
-    ReactiveFormsModule
+    ReactiveFormsModule,
+    RestaurantModule
   ],
   providers: [
     { provide: HTTP_INTERCEPTORS, useClass: FoodLocatorInterceptor, multi: true }
