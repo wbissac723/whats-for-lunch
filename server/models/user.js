4b7be3f63b2cdb53995df52f8e948ee8ab6cb281
Warren <warrenisaac84@gmail.com>
2019-02-18 16:24:31 -0600

    Fixed functionality to save user in database.

--- server/models/user.js
+++ server/models/user.js
@@ -3,6 +3,7 @@ const mongoose = require('mongoose');
 
 
 const userSchema = new mongoose.Schema({
+  _id: mongoose.Schema.Types.ObjectId,
   userName: {
     type: String,
     required: true,
--- server/routes/users.js
+++ server/routes/users.js
@@ -1,71 +1,80 @@
 'use strict';
 
 const express = require('express');
+const mongoose = require('mongoose');
+
 
 const router = express.Router();
 
-const { User, validate } = require('../models/user');
+const {
+  User,
+  validate
+} = require('../models/user');
 
 
 // Add a new user to the database
 router.post('/', async (req, res) => {
 
-    // Validate new user request body against userSchema
-    const { error } = validate(req.body);
+  // Validate new user request body against userSchema
+  const {
+    error
+  } = validate(req.body);
 
-    // Request does not match userSchema
-    if (error) return res.status(400)
-      .json({
-        message: error.details[0].message
-      });
+  // Request does not match userSchema
+  if (error) return res.status(400)
+    .json({
+      message: error.details[0].message
+    });
 
 
-    let user;
+  let user;
 
 
   // Verify user does not already exist by checking email
-    user = await User.findOne({
-        email: req.body.email
-      })
-      .then((data) => {
+  user = await User.findOne({
+      email: req.body.email
+    })
+    .then((data) => {
 
-        if (user) {
-          return res.status(400)
-            .json({
-              message: 'User is already registered.'
-            })
-        }
-      })
-      .catch((err) => {
-          console.log('something messed up')
-        return res.status(500)
+      if (user) {
+        return res.status(400)
           .json({
-            message: 'Internal server error.'
-          });
-      });
-
-
-    // Create a new user from the request body
-    user = new User({
-      user: req.body.userName,
-      email: req.body.email,
-      tribe: req.body.tribe
+            message: 'User is already registered.'
+          })
+      }
+    })
+    .catch((err) => {
+      return res.status(500)
+        .json({
+          message: 'Internal server error.'
+        });
     });
 
-    saveUser(user, res);
+
+  // Create a new user from the request body
+  user = new User({
+    _id: new mongoose.Types.ObjectId(),
+    userName: req.body.userName,
+    email: req.body.email,
+    tribe: req.body.tribe
+  });
+
+  saveUser(user, res);
 });
 
 
 function saveUser(user, res) {
   user.save()
     .then(() => {
+      console.log('Successfully created user.')
       return res.status(201)
         .json({
-          message: 'Succesfully created user.',
+          message: 'Successfully created user.',
           createdUser: user
         });
     })
     .catch((err) => {
+      console.log('Failed to create user: ' + err);
       return res.status(500)
         .json({
           message: 'Unable to create user.'
