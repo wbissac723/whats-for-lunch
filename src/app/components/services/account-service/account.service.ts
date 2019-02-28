f9a7226704ad7530f51e5fbc198a80775d51b333
SirIsaac <warrenbrown@SirIsaac.local>
2019-02-27 18:44:16 -0600

    Created method to create tribe.

--- src/app/components/services/account-service/account.service.ts
+++ src/app/components/services/account-service/account.service.ts
@@ -2,25 +2,31 @@ import { HttpClient } from '@angular/common/http';
 import { Injectable } from '@angular/core';
 
 import { Observable } from 'rxjs';
+import { UserProfile } from '../../user-account/models/user-profile.model';
 
 
 @Injectable({providedIn: 'root'})
 export class AccountService {
-    private usersEndpoint = 'https://meal-vote-api-2.herokuapp.com/api/users';
-    private userProfileEndPoint = 'https://meal-vote-api-2.herokuapp.com/api/users/profile';
+    private baseUrl = 'https://meal-vote-api-2.herokuapp.com/api/users';
 
     constructor(private http: HttpClient) {}
 
     createUser(userName: string, email: string): Observable<any> {
         const body = { userName, email };
 
-        return this.http.post(this.usersEndpoint, body);
+        return this.http.post(this.baseUrl, body);
     }
 
     findUser(email: string): Observable<any> {
         const body = { email };
 
-        return this.http.post(this.userProfileEndPoint, body);
+        return this.http.post(`${this.baseUrl}/profile`, body);
+    }
+
+    createTribe(email: string, tribeName: string): Observable<any> {
+        const body = { email, tribeName };
+
+        return this.http.put(this.baseUrl, body);
     }
 }
 
